21c04b65509187a0244615271e0fa72e9f8e28ad
Alekseiy1980 <129889765+Alekseiy1980@users.noreply.github.com>
2023-07-20 08:35:10 +0300

    Первая задача

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import  random
+'''
+Задача 10: На столе лежат n монеток. Некоторые из них лежат вверх решкой, а некоторые – гербом.
+Определите минимальное число монеток, которые нужно перевернуть, чтобы все монетки были повернуты вверх
+одной и той же стороной. Выведите минимальное количество монет, которые нужно перевернуть
+'''
+# print('Случайное целое число:', end=' ')
+# print(random.randint(0, 10))
 
+manettes = int(input("Введите сколько лежит на столе монеток  "))
+eagle = random.randint(1,manettes)
+up_tails = manettes - eagle
+print(f"manettes - {manettes}, eagle - {eagle}, tails - {up_tails}")
+
+if eagle<up_tails:
+    print(f"переварачиваем eagle - {eagle}")
+else :
+    print(f"переварачиваем tails - {up_tails}")
